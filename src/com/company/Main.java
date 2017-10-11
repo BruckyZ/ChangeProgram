62612744a13b4b5e5e97cf1e391a0f93ccb6b099
BruckyZ <bruckzewge1gmail.com>
2017-10-11 16:19:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] args) {
+        double x, y, z;
+
+        x = 5;
+        y = 9.5;
+
+        z = x * y;
+
+        System.out.println("The product is " + z);
+        System.out.println("The product is " + y);
+    }
+}
